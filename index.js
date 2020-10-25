507873179d436d17bda96490f122159e9c778302
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-10-24 21:31:49 -0300

    Project Started

---
+++ index.js
@@ -0,0 +1,6 @@
+const personalGreeting = "Hello from Gabriel! I am a JavaScript developer!"
+
+console.log(personalGreeting);
+
+const changeBkg = document.getElementById('hero');
+changeBkg.style.background = 'red';
