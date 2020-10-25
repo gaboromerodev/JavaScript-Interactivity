064c710299ae772c0636d0dce314ed1f90922f58
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-10-24 22:15:46 -0300

    Added event listener to a button. Change title text onclick.

--- index.js
+++ index.js
@@ -2,5 +2,23 @@ const personalGreeting = "Hello from Gabriel! I am a JavaScript developer!"
 
 console.log(personalGreeting);
 
+
+//Change the background color of the header
 const changeBkg = document.getElementById('hero');
 changeBkg.style.background = 'red';
+changeBkg.style.color = '#000';
+
+
+// add an event listener to a button
+// change the style and the title on click
+const headerBtn = document.getElementById('headerBtn');
+const myTitle = document.getElementById('myTitle');
+
+headerBtn.addEventListener('click', function(e){
+    this.style.background = 'black';
+    this.style.color = "white";
+    myTitle.textContent = 'I am learning JavaScript';
+    console.log('it was clicked');
+})
+
+
