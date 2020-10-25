447c1a1e053306bf2c49249045149f7abb439a64
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-10-25 12:52:55 -0300

    Create an objetc. Access the value using the 'dot' notation and 'bracket' notation

--- index.js
+++ index.js
@@ -65,3 +65,26 @@ const larry = {
 }
 
 larry.sayGreeting();
+
+// end
+
+// Create an object that describes something of your choosing, like a car or animal.
+// Give that object 5 key/value pairs that include all primitive and complex data types
+// Give the object 1 method that uses the 'this' keyword to console log something from the object when called
+// console.log 2 properties from the object, once with 'dot' notation and once with 'bracket' notation.
+
+const myCar = {
+    brand: "BMW",
+    color: "black",
+    model: 2018,
+    features: ["horn","electrical windows", "automatic doors", "USB", "GPS", "voice assitance"],
+    hasInsurance: true,
+    honkSound: "Cuaaackkk!!!",
+    activateHonkSound: function() {
+        console.log(this.honkSound)
+    }
+}
+
+myCar.activateHonkSound();
+console.log(myCar.brand);
+console.log(myCar["model"]);
